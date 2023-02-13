44700d86bff3442401af7e799944df64eadaa527
Tony Rowan <trowan812@gmail.com>
2023-02-13 20:43:42 +0000

    Put together basic main page

--- app/src/main/java/tech/tonyrowan/pokemon_ev_tracker/MainActivity.kt
+++ app/src/main/java/tech/tonyrowan/pokemon_ev_tracker/MainActivity.kt
@@ -2,10 +2,14 @@ package tech.tonyrowan.pokemon_ev_tracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import tech.tonyrowan.pokemon_ev_tracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
